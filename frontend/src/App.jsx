8bb899070cad0a0001a5e1e50e057db3fb66fef7
Giancarlo Dente <gian20.04@hotmail.com>
2023-09-21 01:10:09 -0400

    componente Footer del Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Layout from "./components/Layout/Layout";
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<h1>Not found page</h1>} />
-
-        {/* Routes manage for user no logged */}
-        <Route path="/" element={<Home />} >
-          <Route index path="/home" element={<Home />} />
-          <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="*" element={<h1>Not found page</h1>} />
+          {/* Routes manage for user no logged */}
+          <Route path="/" element={<Home />} >
+            <Route index path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+// import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import "./Layout.scss";
+
+export default ({children}) => {
+
+    const [selected, setSelected] = useState("");
+    const handleSelection = selection => setSelected(selection);
+
+    const navigation = [
+        { name: "Tienda", path: "" },
+        { name: "Actividades", path: "" },
+        { name: "Acerca de nosotros", path: "" },
+        { name: "Contacto", path: "" }
+    ];
+
+    const socials = [
+        { name: "whatsapp", link: "" },
+        { name: "tiktok", link: "" },
+        { name: "instagram", link: "" },
+        { name: "mail", link: "" }
+    ]
+
+    return (
+        <div className="layout">
+            {/* <Header navigation={navigation}
+                selected={selected} handleSelection={handleSelection} /> */}
+            <main>{children}</main>
+            <Footer links={navigation} active={selected} socials={socials}
+                handler={handleSelection} />
+        </div>
+    );
+}
