c3d5ef211185f1291fcf2a2a53ed90583a92880b
Giancarlo Dente <gian20.04@hotmail.com>
2024-01-20 23:59:30 -0400

    Activities: Añadiendo imagen de fondo

--- frontend/src/pages/Activities.jsx
+++ frontend/src/pages/Activities.jsx
@@ -1,12 +1,11 @@
-
 import Banner from "../components/Banner/Banner";
-
+import BannerImage from "../assets/banners/banner.jpg";
 import { ActivityPage } from "../components/ActivityCard/ActivityPage/ActivityPage";
 
 export default function Activities() {
     return (
       <>
-        <Banner title="ACTIVIDADES" compact={true} />
+        <Banner image={BannerImage} title="ACTIVIDADES" compact={true} />
         <ActivityPage />
       </>
     )
