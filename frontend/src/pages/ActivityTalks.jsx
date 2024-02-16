b8271885a4043df6567391d441008c855711d4f8
Giancarlo Dente <gian20.04@hotmail.com>
2024-02-16 12:11:56 -0400

    algunos arreglos pequenos

--- frontend/src/pages/ActivityTalks.jsx
+++ frontend/src/pages/ActivityTalks.jsx
@@ -2,6 +2,8 @@ import Banner from "../components/Banner/Banner";
 import Layout from "../components/Layout/Layout";
 import TalksCarousel from "../components/TalksCarousel/TalksCarousel";
 import { Talks } from "../utils/dummyTalks";
+import BannerImage from "../assets/banners/banner.jpg";
+
 
 export default function ActivityTalks() {
   const talks = Talks;
@@ -10,9 +12,9 @@ export default function ActivityTalks() {
       <>
         <Layout>
           <Banner
-
-                  title="CHARLAS"
-                  subtitle="TODO A TU ALCANCE"
+            title="CHARLAS"
+            image={BannerImage}
+            compact={true}
           />
           <TalksCarousel product={talks} />
         </Layout>
